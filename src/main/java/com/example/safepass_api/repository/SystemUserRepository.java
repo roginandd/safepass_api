d6d46aa29b3d5c0b35966cce935244a8a03dba8a
roginand <roginandv@gmail.com>
2025-12-08 12:47:08 +0800

    feat(repository): implemented the repository for system user

--- src/main/java/com/example/safepass_api/repository/SystemUserRepository.java
+++ src/main/java/com/example/safepass_api/repository/SystemUserRepository.java
@@ -1,10 +1,12 @@
 package com.example.safepass_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.safepass_api.domain.entity.SystemUser;
 
-public interface SystemUserRepository extends JpaRepository<SystemUser, Long>{
-    SystemUser findByUsername(String username);
-    
+public interface SystemUserRepository extends JpaRepository<SystemUser, String>{
+    Optional<SystemUser> findByUsername(String username);
+
 }
