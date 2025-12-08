383583211ba6963968e9bea193d4b21b40f34d37
roginand <roginandv@gmail.com>
2025-12-08 12:22:52 +0800

    feat(enum): added DocumentType enum

---
+++ src/main/java/com/example/safepass_api/domain/enums/DocumentType.java
@@ -0,0 +1,10 @@
+package com.example.safepass_api.domain.enums;
+
+public enum DocumentType {
+    COMPANY_ID,
+    NBI_CLEARANCE,
+    POLICE_CLEARANCE,
+    COE, // Certificate of Employment
+    PHOTO_2X2,
+    GOV_ID
+}
