c47ac7b8e65239749f355a085e642c7f293d8f02
roginand <roginandv@gmail.com>
2025-12-08 19:09:20 +0800

    feat(repository): added entry log repository

--- src/main/java/com/example/safepass_api/repository/SystemUserRepository.java
+++ src/main/java/com/example/safepass_api/repository/SystemUserRepository.java
@@ -3,9 +3,11 @@ package com.example.safepass_api.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.safepass_api.domain.entity.SystemUser;
 
+@Repository
 public interface SystemUserRepository extends JpaRepository<SystemUser, String>{
     Optional<SystemUser> findByUsername(String username);
 
