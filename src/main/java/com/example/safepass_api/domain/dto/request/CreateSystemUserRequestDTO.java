2531086a5ba466b7ae8dc811c285c087415f2871
roginand <roginandv@gmail.com>
2025-12-08 12:37:26 +0800

    feat(dto): added system user request and response dto

---
+++ src/main/java/com/example/safepass_api/domain/dto/request/CreateSystemUserRequestDTO.java
@@ -0,0 +1,10 @@
+package com.example.safepass_api.domain.dto.request;
+
+public class CreateSystemUserRequestDTO {
+    private String username;
+    private String password;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+}
