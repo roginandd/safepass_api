79bcc9bb203221e74fecf96fb11c16410d959852
roginand <roginandv@gmail.com>
2025-12-09 15:27:32 +0800

    refactor(dto): specified which id belong to the foreign key for the dto of {typeof role (e.g stakeholder, visitor)}entrylog

--- src/main/java/com/example/safepass_api/domain/dto/response/StakeHolderEntryLogResponseDTO.java
+++ src/main/java/com/example/safepass_api/domain/dto/response/StakeHolderEntryLogResponseDTO.java
@@ -11,8 +11,8 @@ import lombok.Data;
 @Data
 @Builder
 public class StakeHolderEntryLogResponseDTO {
-    private String id;
-
+    private String stakeHolderId;
+    
     private String firstName;
     private String middleName;
     private String lastName;
