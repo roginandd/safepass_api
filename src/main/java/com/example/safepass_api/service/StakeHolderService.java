db6d084a480ef1083db26be53e59f4ec176d4366
roginand <roginandv@gmail.com>
2025-12-09 18:30:48 +0800

    feat(stakeholder): implemented an updateStakeHoler by status method

--- src/main/java/com/example/safepass_api/service/StakeHolderService.java
+++ src/main/java/com/example/safepass_api/service/StakeHolderService.java
@@ -4,13 +4,19 @@ import java.util.List;
 import java.util.Optional;
 
 import com.example.safepass_api.domain.dto.request.CreateStakeHolderRequestDTO;
+import com.example.safepass_api.domain.dto.request.CreateStakeHolderWithDocumentsRequestDTO;
 import com.example.safepass_api.domain.dto.request.UpdateStakeHolderRequestDTO;
 import com.example.safepass_api.domain.dto.response.StakeHolderResponseDTO;
+import com.example.safepass_api.domain.enums.StakeHolderStatus;
 
 public interface StakeHolderService {
     List<StakeHolderResponseDTO> getAllStakeHolders();
     Optional<StakeHolderResponseDTO> getStakeHolderById(String id);
     Optional<StakeHolderResponseDTO> createStakeHolder(CreateStakeHolderRequestDTO createRequest);
+    Optional<StakeHolderResponseDTO> createStakeHolderWithDocuments(CreateStakeHolderWithDocumentsRequestDTO createRequest);
     Optional<StakeHolderResponseDTO> updateStakeHolder(String id, UpdateStakeHolderRequestDTO updateRequest);
+    
+    Optional<StakeHolderResponseDTO> updateStakeHolderByStatus(String id, StakeHolderStatus status);
+
     boolean deleteStakeHolder(String id);
 }
--- src/main/java/com/example/safepass_api/service/impl/DocumentServiceImpl.java
+++ src/main/java/com/example/safepass_api/service/impl/DocumentServiceImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,6 +31,8 @@ public class DocumentServiceImpl implements DocumentService{
     private final S3Service s3Service;
     private final DocumentMapper documentMapper;
 
+    @Value("${file.root-url}")
+    private String rootUrl;
     /**
      * Upload document for a stake holder
      */
@@ -45,6 +48,7 @@ public class DocumentServiceImpl implements DocumentService{
         // Upload to S3
         String fileName = s3Service.uploadFile(file, uploadRequest.getStakeHolderId());
 
+        
         // Create document record
         Documents document = Documents.builder()
                 .documentType(uploadRequest.getDocumentType())
@@ -52,6 +56,7 @@ public class DocumentServiceImpl implements DocumentService{
                 .stakeHolder(stakeHolder)
                 .build();
 
+        document.setDocumentUrl(rootUrl + document.getDocumentUrl());
         Documents savedDocument = documentRepository.save(document);
         return documentMapper.toResponseDTO(savedDocument);
     }
